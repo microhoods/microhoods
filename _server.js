9767f3ffae2e0e6c11755f4ba495f916f6d20ca6
Austen Talbot <austentalbot@gmail.com>
2014-07-17 11:50:09 -0700

    Updated request handlers to save to DB

--- _server.js
+++ _server.js
@@ -5,5 +5,10 @@ app.start(function() {
   //   query.on('row', function(row) {
   //   console.log(row.username);
   // });
+  var query = client.query("SELECT * FROM TAGS;");
+  query.on('row', function(row) {
+    console.log(row);
+  });
+
   console.log('server running at: ' + app.info.uri); 
 });
--- server/users/userController.js
+++ server/users/userController.js
@@ -1,5 +1,24 @@
 var settings = require('../config/settings.js');  
 
+String.prototype.supplant = function(o) {
+  return this.replace(/{([^{}]*)}/g,
+    function (a, b) {
+      var r = o[b];
+      return typeof r === 'string' || typeof r === 'number' ? r : a;
+    }
+  );
+};
+
+var addTag=function(user, tag, coord) {
+  //coordinates passed in as array of [lat, lng]
+  client.query("INSERT INTO TAGS (user_id, tag, coordinates) VALUES( \
+    (SELECT user_id FROM USERS \
+    WHERE username='{userName}'), \
+    '{tagName}', \
+    '{coordinates})');".supplant({userName: user, tagName: tag, coordinates: coord})
+  );
+}
+
 module.exports = { 
   index: { 
     handler: function(request, reply) { 
@@ -9,6 +28,15 @@ module.exports = {
   addTag: {
     handler: function(request, reply) {
       console.log(request.payload);
+      //loop over each coordinate
+      for (var coord in request.payload) {
+        //loop over each tag
+        for (var i=0; i<request.payload[coord].length; i++) {
+          //add tag
+          addTag('Forest', request.payload[coord][i], coord);
+          console.log('saved!');
+        }
+      }
     }, 
     payload: {
       parse: true
@@ -17,15 +45,15 @@ module.exports = {
     // handler: function(request, reply) {
     //   function(userName, tagName, coordinates) {
     //     //coordinates passed in as array of [lat, lng]
-    //     client.query('INSERT INTO TAGS (user_id, tag, coordinates) VALUES( \
-    //       (SELECT user_id FROM USERS \
-    //       WHERE username="{userName}"), \
-    //       "{tagName}", \
-    //       "{coordinates})");'.supplant({
-    //         // userName: userName, 
-    //         userName: 'Forest', 
-    //         tagName: tagName, 
-    //         coordinates: coordinates
+        // client.query('INSERT INTO TAGS (user_id, tag, coordinates) VALUES( \
+        //   (SELECT user_id FROM USERS \
+        //   WHERE username="{userName}"), \
+        //   "{tagName}", \
+        //   "{coordinates})");'.supplant({
+        //     // userName: userName, 
+        //     userName: 'Forest', 
+        //     tagName: tagName, 
+        //     coordinates: coordinates
     //     }));
     //   }
     // }
