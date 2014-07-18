d753adec24d05b4b22adf55ccf8db48e23446478
Adam Price <aesprice@gmail.com>
2014-07-18 13:31:13 -0700

    centering microhoods logo

--- www/js/microhoods.home.js
+++ www/js/microhoods.home.js
@@ -14,9 +14,10 @@ var app = angular.module('microhoods.home', [])
   };
 
   var height=$window.document.body.scrollHeight*.90;
-  $window.document.getElementById("map").style.height=height.toString()+'px'
+  $window.document.getElementById("map").style.height=height.toString()+'px';
   var topPos=$window.document.body.scrollHeight*.0264;
-  $window.document.getElementById("map").style.top=topPos.toString()+'px'
+  $window.document.getElementById("map").style.top=topPos.toString()+'px';
+  $window.document.getElementById("title").style.height=topPos.toString()+'px';
 
   //initialize map to SF
   var map = L.map('map', {zoomControl: false, attributionControl: false, maxBounds: [[37.7, -122.65], [37.85, -122.3]], minZoom: 12}).setView([37.789, -122.414], 14);
